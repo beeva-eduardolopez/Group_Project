c3abc56287bba7b3c42e8a07efe6e3c09d6e5f9b
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-08 12:09:29 -0500

    add router

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,14 +1,18 @@
 import {App, events, register, routing, web} from 'platypus';
 import HomeViewControl from '../viewcontrols/home/home.vc';
+import AboutViewControl from '../viewcontrols/about/about.vc';
+import PromotionsViewControl from '../viewcontrols/promotions/promotions.vc';
 
 export default class MyApp extends App {
     constructor(router: routing.Router, config: web.IBrowserConfig) {
         super();
 
-		config.routingType = config.STATE;
+        config.routingType = config.STATE;
 
         router.configure([
-            { pattern: '', view: HomeViewControl }
+            { pattern: '', view: HomeViewControl },
+            { pattern: 'about', view: AboutViewControl },
+            { pattern: 'promotions', view: PromotionsViewControl }
         ]);
     }
 
