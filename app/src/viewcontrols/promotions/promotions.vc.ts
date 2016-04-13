5895d7af81373bbdb85205b3b4edb774e5a035e3
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-13 12:07:15 -0500

    Added modal and delete index.html problems wirh extra images and extra code...

--- app/src/viewcontrols/promotions/promotions.vc.ts
+++ app/src/viewcontrols/promotions/promotions.vc.ts
@@ -7,7 +7,24 @@ import HowToPlayViewController from '../../viewcontrols/howtoplay/howtoplay.vc';
 export default class PromotionsViewControl extends BaseViewControl {
     templateString: string = require('./promotions.vc.html');
 
-    context: any = {};
+    context: any = {
+        showModal: false,
+        name: '',
+        last: '',
+        email: '',
+        zip: '',
+        team:''
+    };
+
+    toggleModal() {
+        this.context.showModal = !this.context.showModal;
+        this.context.name = '';
+        this.context.last = '';
+        this.context.email = '';
+        this.context.zip = '';
+        this.context.team = '';
+
+    }
 }
 
 register.viewControl('promotions-vc', PromotionsViewControl);
