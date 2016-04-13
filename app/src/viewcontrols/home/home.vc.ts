c2c9508ebfe9a3c827e893c0aecb0fc2f387b3d8
Patrick Locke <patrick.s.locke@gmail.com>
2016-04-12 21:08:39 -0500

    css changes for home view and footer

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -8,7 +8,11 @@ export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
     context: any = {
+        
+        
     };
+  
+    
 }
 
 register.viewControl('home-vc', HomeViewControl);
