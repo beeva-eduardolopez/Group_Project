a80321c40899ad50d75de019299e1a45fbd7f029
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-15 10:29:44 -0500

    delete videos and over view controller

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -3,8 +3,6 @@ import HomeViewControl from '../viewcontrols/home/home.vc';
 import AboutViewControl from '../viewcontrols/about/about.vc';
 import PromotionsViewControl from '../viewcontrols/promotions/promotions.vc';
 import HowToPlayViewController from '../viewcontrols/howtoplay/howtoplay.vc';
-import OverviewViewController from '../viewcontrols/overview/overview.vc';
-import VideosViewController from '../viewcontrols/videos/videos.vc';
 
 export default class MyApp extends App {
     constructor(router: routing.Router, config: web.IBrowserConfig) {
@@ -16,9 +14,7 @@ export default class MyApp extends App {
             { pattern: '', view: HomeViewControl },
             { pattern: 'about', view: AboutViewControl },
             { pattern: 'promotions', view: PromotionsViewControl },
-            { pattern: 'howtoplay', view: HowToPlayViewController },
-            { pattern: 'overview', view: OverviewViewController },
-            { pattern: 'videos', view: VideosViewController }
+            { pattern: 'howtoplay', view: HowToPlayViewController }
         ]);
     }
 
